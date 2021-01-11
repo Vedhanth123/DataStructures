f3251c037dc60122b26bb7e44433894ef7968d66
Vedhanth123 <67219089+Vedhanth123@users.noreply.github.com>
2021-01-11 12:23:16 +0530

    Create dynamic_programming.c

---
+++ Data_structures/DP/dynamic_programming.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 19124-CM-111 Vedhanth
+void main()
+{
+    int *pointer;
+
+    // asking user for amount of memory to be allocated
+    int n;
+    printf("Enter the size of memory to be allocated: \n");
+    scanf("%d", &n);
+
+    // allocating memory using malloc
+    pointer = (int*) malloc(sizeof(int) * n);
+
+    if(pointer == NULL)
+        printf("Memory not allocated: \n");
+    
+    else
+    {
+        printf("Memory allocated: \n");
+
+        // inserting values into memory
+        for(int i = 0; i < n; i++)
+            pointer[i] = i;
+
+        // accessing values from memory
+        for(int i = 0; i < n; i++)
+            printf("%d\n",*(pointer + i));
+
+    }
+
+    // allocating memory using calloc
+    pointer = (int*) calloc(n, sizeof(int));
+
+    if(pointer == NULL)
+        printf("Memory not allocated: \n");
+    
+    else
+    {
+        printf("Memory allocated: \n");
+
+        // inserting values into memory
+        for(int i = 0; i < n; i++)
+            pointer[i] = i;
+
+        // accessing values from memory
+        for(int i = 0; i < n; i++)
+            printf("%d\n",*(pointer + i));
+
+    }
+
+    printf("Enter the memory to be reallocated: \n");
+    scanf("%d",&n);
+
+    // reallocating memory
+    pointer = (int*) realloc(pointer, n*sizeof(int));
+
+    // checking if memory reallocated
+    if(pointer == NULL)
+        printf("Memory not reallocated: \n");
+    
+    else
+    {
+        printf("Memory reallocated:\n");
+
+        for(int i = 0; i < n; i++)
+            pointer[i] = i;
+
+        // accessing values from memory
+        for(int i = 0; i < n; i++)
+            printf("%d\n",*(pointer + i));
+
+    }   
+
+    // deallocating memory
+    free(pointer);
+
+    printf("Trying to access memory after freeing memory: \n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d\n",pointer[i]);
+    }
+}
