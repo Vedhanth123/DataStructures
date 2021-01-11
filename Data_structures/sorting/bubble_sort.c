27e53f46b0ecb2f7cca094949b9d1192fab9f555
Vedhanth123 <67219089+Vedhanth123@users.noreply.github.com>
2021-01-11 12:54:15 +0530

    Update bubble_sort.c

--- Data_structures/sorting/bubble_sort.c
+++ Data_structures/sorting/bubble_sort.c
@@ -7,22 +7,13 @@ void main()
 // bubble sort is swapping elements untill and unless it gets sorted
 {
     // initializing array
-    int array[5];
-
-    // asking user for input
-    printf("Please enter values in the array: \n");
-
-    // reading the values from the user
-    for (int i = 0; i < 5; i ++)
-    {
-        scanf("%d",&array[i]);
-    }
+    int array[5] = {99,88,77,66,55};
 
     // looping the swapping phases
     for (int i = 0; i < 5; i++)
     {
         // swapping the elements in a loop
-        for(int j = 0; j < 5; j++)
+        for(int j = 0; j < 4; j++)
         {
             // checking if the neighbour elements are greater than the previous one
             if(array[j] > array[j+1])
