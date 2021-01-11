7e67b51fa8cae28c4bced1607fef36d92fcf432b
Vedhanth123 <67219089+Vedhanth123@users.noreply.github.com>
2021-01-11 13:22:52 +0530

    Update array_of_pointers.c

--- Data_structures/Pointers/array_of_pointers.c
+++ Data_structures/Pointers/array_of_pointers.c
@@ -2,12 +2,18 @@
 
 void main()
 {
+    // creating array of pointers
+    
+    // array of pointers helps us to create any size string without defining the size of string
     char *p[3] = {"cricket","football","basketball"};
 
+    // iterator to traverse array of pointers
     int i;
 
+    // traverse array of pointers
     for (i = 0; i < 5; i++)
     {
-        printf("%s,  %s\n",*(p+i));
+        
+        printf("%s,\n",*(p+i));
     }   
 }
