ee5dfbccc993376aa2b046e5838eb954e1111e2b
Vedhanth123 <67219089+Vedhanth123@users.noreply.github.com>
2021-01-11 13:54:19 +0530

    Create calloc.c

---
+++ Data_structures/DP/calloc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+
+// allocating memory using calloc
+    pointer = (int*) calloc(n, sizeof(int));
+
+    if(pointer == NULL)
+        printf("Memory not allocated: \n");
+    
+    else
+    {
+        printf("Memory allocated: \n");
+
+        // inserting values into memory
+        for(int i = 0; i < n; i++)
+            pointer[i] = i;
+
+        // accessing values from memory
+        for(int i = 0; i < n; i++)
+            printf("%d\n",*(pointer + i));
+
+    }
+}
