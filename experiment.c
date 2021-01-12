7dcd68fa586cd3e5862058fe853f6b9e7715eb1a
Vedhanth123 <gvedhanth123@outlook.com>
2021-01-12 12:59:04 +0530

    Add files via upload

---
+++ experiment.c
@@ -0,0 +1,42 @@
+// bubble sort bc O(n), ac O(n^2), wc O(n^2)
+
+// insetion sort bc O(n), ac O(n^2), wc O(n^2)
+
+// selection sort bc O(n), ac O(n^2), wc O(n^2)
+
+// Quick sort bc O(n), ac O(log2(n)*n), wc O(n^2) sc O(1) or O(log2n)
+
+// Merge sort bc O(n), ac O(log2(n)*n), wc O(log2(n)*n) // 
+
+
+// n = 100 
+
+// // bs = bc 100, ac 10000, wc 10000
+
+// // Qs = bc 100. ac 600, wc 10000
+
+// // Ms = bc 100, ac 600, wc 600
+
+// // searching 
+
+// 9,5,8,3,2,1,0,3,4
+
+// n = 100
+
+// bc O(1)
+
+// ac O(n)
+
+// wc O(n)
+
+// // binary searching
+
+// 5
+
+// 1,2,3,4,5,6,7,8,9
+
+// bc O(1)
+
+// ac O(log2(n))
+
+// wc O(log2(n))
