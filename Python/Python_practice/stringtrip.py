5dd26b902ae6542bfe34045abeca7130cc81b002
Vedhanth123 <gvedhanth123@outlook.com>
2021-01-12 19:50:01 +0530

    Delete Python directory

--- Python/Python_practice/stringtrip.py
+++
@@ -1,9 +0,0 @@
-# python prog to implement string strip
-
-def to_camel_case(string):
-    
-    new_string = string.split('-' or "_")
-
-    print(new_string)
-
-to_camel_case("the-stealth-warrior")
--- Python/Python_practice/tables.py
+++
@@ -1,4 +0,0 @@
-# program to print multiplication tables
-
-for x in range(1,11):
-    print(f"{1} *{x} = {1*x}   {2} *{x} = {2*x}     {3} *{x} = {3*x}     {4} *{x} = {4*x}     {5} *{x} = {5*x}     {6} *{x} = {6*x}")
