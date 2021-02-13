f208a755ce8aa13fc752ac2b1a1196476f94185b
Vedhanth <gvedhanth123@outlook.com>
2021-02-13 17:15:09 +0530

    Update linear_search.c

--- Data_structures/Search/linear_search.c
+++ Data_structures/Search/linear_search.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
+#include <conio.h>
+
 void main()
 {
-    // 19124-CM-111 Vedhanth
+    
     int array[5] = {4,10,40,6,8};
-
+    
     // asking user for the key
     int key;
     printf("Enter the number which you want to search in the array:\n");
     scanf("%d",&key);
 
+    // checking if element found or not
     int found = 0;
 
     int i = 0;
 
     for(i = 0; i < 5; i ++)
     {
+        
         if(key == array[i])
         {
             found = 1;
@@ -26,5 +30,7 @@ void main()
         printf("key found at %d place:\n",i+1);
     else
         printf("key not found:\n");
+    
+    getch();
 
 }
