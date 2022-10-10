21c895960649eafe72fe8f0c0e33743e03452113
Vedhanth <gvedhanth123@outlook.com>
2022-10-10 22:29:29 +0530

    Second Comiit

--- DS-OU/LinkedList.c
+++ DS-OU/LinkedList.c
@@ -6,7 +6,27 @@
 // 2) pointer
 // 3) index variables inside it.
 
+struct LinkedList
+{
+    int data;
+    struct LinkedList *pointer;
+};
+
+// Create a pointer which points to the head of the node
+struct LinkedList *head = NULL;
+struct LinkedList *node;
+
 int main()
 {
-    
+    // write a function to create a new Node
+    void CreateNewNode(int data) {
+
+        node = (struct LinkedList*) malloc(struct LinkedList);
+        if(head == NULL) {
+            head = node;
+        }
+    } 
+
+    CreateNewNode(10);
+
 }
