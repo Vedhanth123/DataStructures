037f48ac811f1b02f467c825c1b659c6e5894f65
Vedhanth123 <gvedhanth123@gmail.com>
2022-11-10 20:10:12 +0530

    Merged

--- DS-OU/MergeSort.c
+++ DS-OU/MergeSort.c
@@ -1,21 +1,45 @@
 // take an array
 #include <stdio.h>
 
-int array[10] = {9,8,7,6,5,4,3,2,1,0};
+int array[10] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 
 // write a function to divide the array every time into 2 parts till the size of array is 1
-void MergeSort(int* array, int left, int right) { 
-    
-    if(left >= right) {
+void MergeSort(int *array, int left, int right)
+{
+
+    if (left >= right)
+    {
         return 0;
     }
 
     int mid = (left + right) / 2;
     MergeSort(array, left, mid);
-    MergeSort(array, mid+1, right);
+    MergeSort(array, mid + 1, right);
 
-    int merged_sorted_array_index = 0
-    
-    while()
+    int merged_sorted_array_index = 0;
+    int swapper = 0;
 
+    while (left < mid && mid + 1 <= right)
+    {
+        if (array[left] > array[right])
+        {
+            swapper = array[right];
+            array[right] = array[left];
+            array[left] = swapper;
+            left++;
+        }
+        else
+        {
+            swapper = array[right];
+            array[right] = array[left];
+            array[left] = swapper;
+            right++;
+        }
+    }
+    if (left == mid)
+    {
+        while (mid + 1 <= right)
+        {
+        }
+    }
 }
--- DS-OU/MergeSort.py
+++ DS-OU/MergeSort.py
@@ -1,38 +1,59 @@
 # create an array
-array = [9,8,7,6,5,4,3,2,1,0]
+array = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
 
 def MergeSort(array):
-    
-    if(len(array) == 1):
+
+    # stop recursion of the array if the size of the array is 1
+    if (len(array) == 1):
         return array
+
     # divide the array into 2 parts every time till the size of array is 1
     left = MergeSort(array[:len(array)//2])
     right = MergeSort(array[len(array)//2:])
 
-
+    # creating indexes for the left and right arrays
     left_index = 0
     right_index = 0
 
     # creating another array to store merged and sorted array
     merged_sorted_array = []
 
-    while(left_index < len(left) and right_index < len(right)):
-        if(left[left_index] < right[right_index]):
+    # traversing though left array and making sure that we don't over traverse the left and right array
+    while (left_index < len(left) and right_index < len(right)):
+
+        # if element of the left array is less that right array
+        if (left[left_index] < right[right_index]):
+
+            # insert the element of the left array into merged and sorted array
             merged_sorted_array.append(left[left_index])
             left_index += 1
         else:
+
+            # insert the element of the right array into merged and sorted array
             merged_sorted_array.append(right[right_index])
             right_index += 1
-    
-    if(left_index == len(left)):
-        while(right_index < len(right)):
+
+    # checking if left array is fully traversed
+    if (left_index == len(left)):
+
+        # traversing till the right array is completely traversed
+        while (right_index < len(right)):
+
+            # inserting the elements of the right array into merged and sorted array
             merged_sorted_array.append(right[right_index])
             right_index += 1
+
+    # if the left array is not fully traversed then traverse the array properly
     else:
-        while(left_index < len(left)):
+
+        # traversing till the left array is completely traversed
+        while (left_index < len(left)):
+
+            # inserting the elements of the left array into merged and sorted array
             merged_sorted_array.append(left[left_index])
             left_index += 1
-            
+
     return merged_sorted_array
 
 
