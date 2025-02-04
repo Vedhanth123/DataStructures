47596d1daaa46d8d58ef05b34bf1529889c650ad
Vedhanth <gvedhanth123@outlook.com>
2025-02-04 18:33:59 +0530

    Time: 270 ms (33.09%), Space: 20.1 MB (75.18%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,32 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
-        # start from top left part of the grid and search for 1's
-        # if found 1 then increment counter
-        # do a bfs and change all the values to zero in the coming path
-        # return the counter
+
+        queue = []
 
         islands = 0
 
-        queue = deque([])
-        directions = [(0,1),(0,-1),(1,0),(-1,0)]
-
+        directions = [(0,-1), (0,1), (1,0), (-1,0)]
         for x in range(len(grid)):
-
             for y in range(len(grid[0])):
 
                 if(grid[x][y] == '1'):
                     queue.append((x,y))
                     islands += 1
+
                     while(queue):
 
-                        curr = queue.popleft()
-                        for r,c in directions:
-                            
-                            if(0 <= curr[0]+r < len(grid) and 0 <= curr[1]+c < len(grid[0]) and grid[curr[0]+r][curr[1]+c] == '1'):
+                        curr = queue.pop(0)
+
+                        for r, c in directions:
+
+                            if(0<= curr[0]+r < len(grid) and 0 <= curr[1]+c <len(grid[0]) and grid[curr[0]+r][curr[1]+c] == '1'):
+
                                 queue.append((curr[0]+r, curr[1]+c))
-                                grid[curr[0]+r][curr[1]+c] = '0'
+                                grid[curr[0]+r][curr[1]+c] = 0
         
+
         return islands
-                            
+
+
+
+
