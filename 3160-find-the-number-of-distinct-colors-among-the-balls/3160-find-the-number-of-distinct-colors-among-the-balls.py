de10fe3bd41aef6bdf0b10c4d5d6cab13cd4fa07
Vedhanth <gvedhanth123@outlook.com>
2025-02-07 11:37:42 +0530

    Time: 80 ms (42.86%), Space: 63.1 MB (80.8%) - LeetHub

--- 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.py
+++ 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.py
@@ -2,19 +2,19 @@ class Solution:
     def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
         
         color_map = defaultdict(int)
-        balls_array = [0] * (limit+1)
+        balls_map = defaultdict(int)
         answer = []
 
         for balls, colors in queries:
 
-            if(balls_array[balls] != 0):
+            if(balls_map[balls] != 0):
 
-                color_map[balls_array[balls]] -= 1
-                if(color_map[balls_array[balls]] == 0):
-                    del color_map[balls_array[balls]]
+                color_map[balls_map[balls]] -= 1
+                if(color_map[balls_map[balls]] == 0):
+                    del color_map[balls_map[balls]]
 
 
-            balls_array[balls] = colors
+            balls_map[balls] = colors
             color_map[colors] += 1 
             answer.append(len(color_map))
 
