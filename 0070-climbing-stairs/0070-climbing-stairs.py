ff8ecc160ea5c31b8392984ec48327814a9ec0e8
Vedhanth <gvedhanth123@outlook.com>
2025-02-13 14:24:19 +0530

    Time: 0 ms (100%), Space: 17.9 MB (7.84%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -11,15 +11,26 @@ class Solution:
         # return recursion(n)
 
         # def memoization
-        mem = [1,1] + [-1] * (n-1)
-        def rec2(n):
+        # mem = [1,1] + [-1] * (n-1)
+        # def rec2(n):
 
-            if(mem[n] != -1):
-                return mem[n]
-            else:
-                mem[n] = rec2(n-1) + rec2(n-2)
-                return mem[n]
+        #     if(mem[n] != -1):
+        #         return mem[n]
+        #     else:
+        #         mem[n] = rec2(n-1) + rec2(n-2)
+        #         return mem[n]
     
-        rec2(n)
-        return mem[n]
+        # rec2(n)
+        # return mem[n]
+
+        # iteration
+        prev1 = 0
+        prev2 = 1
+        curr = 0
+        for x in range(1,n+1):
+            curr = prev2 + prev1
+            prev1 = prev2
+            prev2 = curr
+        
+        return curr
             
