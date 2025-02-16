ca94c630e281bb2fcd6b867cf8bffb324e58db2a
Vedhanth <gvedhanth123@outlook.com>
2025-02-16 21:23:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,33 +1,33 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         
-        
-        for x in range(len(matrix)):
+        r = len(matrix)
+        c = len(matrix[0])
 
-            y = x
-            z = 0
-            diag_elem = matrix[x][z]
-            while(y < len(matrix) and z < len(matrix[0])):
+        # lt
+        for lt in range(r):
+            ltr = r
+            ltc = 0
+            diag = matrix[ltr][0]
 
-                if(diag_elem != matrix[y][z]):
+            while(ltr < r and ltc < c):
+                if(matrix[ltr][ltc] != diag):
                     return False
-                y += 1
-                z += 1
+            
+                ltr += 1
+                ltc += 1
         
-        for x in range(1,len(matrix[0])):
-
-            y = x
-            z = 0
-            diag_elem = matrix[z][y]
-            while(z < len(matrix) and y < len(matrix[0])):
-
-                if(diag_elem != matrix[z][y]):
+        # ut
+        for ut in range(1, c):
+            utc = ut
+            utr = 0
+            diag = matrix[utr][utc]
+            
+            while(utr < r and utc < c):
+                if(matrix[utr][utc]!= diag):
                     return False
                 
-                y += 1
-                z += 1
-
-            
+                utr += 1
+                utc += 1
+        
         return True
-            
-                    
