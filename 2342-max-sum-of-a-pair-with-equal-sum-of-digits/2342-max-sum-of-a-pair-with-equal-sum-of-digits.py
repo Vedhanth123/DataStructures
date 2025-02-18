6d8b5c2dcbf5d17f073c37e3fc1dbd89f4c72b5d
Vedhanth <gvedhanth123@outlook.com>
2025-02-18 23:34:27 +0530

    Time: 290 ms (78.33%), Space: 33.4 MB (82.18%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -1,31 +1,24 @@
 class Solution:
     def maximumSum(self, nums: List[int]) -> int:
-
-        max_sum = -1
+        
         sum_of_digits = {}
+        answer = -1
         for x in range(len(nums)):
 
-            temp = nums[x]
             digit_sum = 0
-            while(temp):
-                digit_sum += temp % 10
-                temp //= 10
+            num = nums[x]
+            while(num != 0):
+                digit_sum += num % 10
+                num //= 10
             
             if(digit_sum in sum_of_digits):
                 heapq.heappush(sum_of_digits[digit_sum], -nums[x])
             else:
                 sum_of_digits[digit_sum] = [-nums[x]]
         
-        
-        for key, val in sum_of_digits.items():
+        for key,val in sum_of_digits.items():
 
             if(len(val) >= 2):
-                n1 = -heapq.heappop(val)
-                n2 = -heapq.heappop(val)
-                max_sum = max(max_sum, n1+n2)
+                answer = max(answer, -heapq.heappop(val) + (-heapq.heappop(val)))
         
-        return max_sum
-                
-
-            
-        
+        return answer
