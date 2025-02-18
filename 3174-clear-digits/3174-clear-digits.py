b2430c3ecb2bdf70263405627176d5f39119ca3e
Vedhanth <gvedhanth123@outlook.com>
2025-02-18 21:44:50 +0530

    Time: 3 ms (28.55%), Space: 17.6 MB (79.1%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.py
+++ 3174-clear-digits/3174-clear-digits.py
@@ -1,24 +1,13 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
         
-        # 2) excluded indices
-        sett = SortedSet()
-
-        # 1) find the digit
-        for x in range(len(s)):
-
-            if(s[x].isnumeric()):
-                sett.add(x)
-                y = x-1
-                for y in range(x-1,-1,-1):
-                    if(y not in sett and s[y].isalpha()):
-                        sett.add(y)
-                        break
-        
-        answer = ""
+        stack = []
 
         for x in range(len(s)):
-            if(x not in sett):
-                answer += s[x]
+            
+            if(s[x].isalpha()):
+                stack.append(s[x])
+            else:
+                stack.pop()
         
-        return answer
+        return "".join(stack)
