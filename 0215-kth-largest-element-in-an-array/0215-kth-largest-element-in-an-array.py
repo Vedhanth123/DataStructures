8e43930298cf5463112f435df11cf28f30b72d4e
Vedhanth <gvedhanth123@outlook.com>
2025-02-26 14:26:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -4,9 +4,8 @@ class Solution:
         max_heap = [-x for x in nums]
 
         heapq.heapify(max_heap)
-        
-        for x in range(k-1):
 
+        for x in range(k-1):
             heapq.heappop(max_heap)
         
-        return -heapq.heappop(max_heap)
+        return heapq.heappop(max_heap)
