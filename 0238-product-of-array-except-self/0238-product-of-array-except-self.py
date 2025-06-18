53207ca42a2e8ed58dda560a010bd24a9877d2bc
Vedhanth <gvedhanth123@outlook.com>
2025-06-18 21:11:10 +0530

    Leetcode: 0238-product-of-array-except-self

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,19 +1,18 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-
-        res = [1] * (len(nums))
-
+        
         prefix = 1
-        for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
-        
         postfix = 1
-        for i in range(len(nums) -1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-        
-        return res
+        result = [1] * len(nums)
 
-obj = Solution()
-obj.productExceptSelf([1,2,3,4])
+        for x in range(len(nums)):
+
+            result[x] = prefix
+            prefix *= nums[x]
+        
+        for x in range(len(nums)-1,-1,-1):
+
+            result[x] *= postfix
+            postfix *= nums[x]
+
+        return result
