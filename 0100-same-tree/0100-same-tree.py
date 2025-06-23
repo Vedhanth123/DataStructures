820c5707aa6d5c99b99cc81af2bc9216d6e4d2b8
Vedhanth <gvedhanth123@outlook.com>
2025-06-23 10:43:15 +0530

    Leetcode: 0100-same-tree

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,34 +6,32 @@
 #         self.right = right
 class Solution:
 
-    def inorder(self, tree, inorder):
-
-        if(tree):
-            self.inorder(tree.left, inorder)
-            inorder.append(tree.val)
-            self.inorder(tree.right, inorder)
-        else:
-            inorder.append(None)
-        return inorder
-
-    def preorder(self, tree, preorder):
-        if(tree):
-            preorder.append(tree.val)
-            self.preorder(tree.left, preorder)
-            self.preorder(tree.right, preorder)
-        else:
-            return preorder.append(None)
-        return preorder
-
-    def postorder(self, tree, postorder):
-        if(tree):
-            postorder.append(tree.val)
-            self.postorder(tree.left, postorder)
-            self.postorder(tree.right, postorder)
-        else:
-            return postorder.append(None)
-        return postorder
 
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
+        preorder = []
 
-        return self.inorder(p, []) == self.inorder(q, []) and self.preorder(p, []) == self.preorder(q, [])
+        def inorder(root,ino):
+            if(root):
+                inorder(root.left,ino)
+                ino.append(root.val)
+                inorder(root.right,ino)
+            else:
+                ino.append(None)
+            return ino
+        
+        def preorder(root,pre):
+            if(root):
+                pre.append(root.val)
+                preorder(root.left,pre)
+                preorder(root.right,pre)
+            else:
+                pre.append(None)
+            return pre
+        
+        ip = inorder(p, [])
+        iq = inorder(q, [])
+        pp = preorder(p, [])
+        pq = preorder(q, [])
+    
+        return ((ip == iq) and (pp == pq))
