a46d3b19b3c0c82f1af5b1082aedbea4af77270d
Vedhanth <gvedhanth123@outlook.com>
2025-06-23 11:44:28 +0530

    Leetcode: 0002-add-two-numbers

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -6,55 +6,41 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
-        sum_ = 0
-        carry = 0
+        new_list = ListNode()
+        temp = new_list
 
         p1 = l1
         p2 = l2
 
-        new_head = ListNode()
-        temp = new_head
+        carry = 0
 
         while(p1 and p2):
 
             sum_ = p1.val + p2.val + carry
-            if(sum_ >= 10):
-                carry = 1
-            else:
-                carry = 0
-
-            temp.next = ListNode(sum_ % 10)
+            carry = 1 if sum_ >= 10 else 0
+            temp.next = ListNode(sum_%10)
             temp = temp.next
             p1 = p1.next
             p2 = p2.next
-
+        
         while(p1):
-            
             sum_ = p1.val + carry
-            if(sum_ >= 10):
-                carry = 1
-            else:
-                carry = 0
-            
-            temp.next = ListNode(sum_ % 10)
+            carry = 1 if sum_ >= 10 else 0
+            temp.next = ListNode(sum_%10)
             temp = temp.next
             p1 = p1.next
-        
+
         while(p2):
 
             sum_ = p2.val + carry
-            if(sum_ >= 10):
-                carry = 1
-            else:
-                carry = 0
-            
-            temp.next = ListNode(sum_ % 10)
+            carry = 1 if sum_ >= 10 else 0
+            temp.next = ListNode(sum_%10)
             temp = temp.next
-            p2 = p1.next
+            p2 = p2.next
 
-        
         if(carry):
             temp.next = ListNode(carry)
-            temp = temp.next
+    
+        
+        return new_list.next
 
-        return new_head.next
