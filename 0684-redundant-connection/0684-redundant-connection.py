1b87135d79e9304bedbef4a83e22c37eff526eff
Vedhanth <gvedhanth123@outlook.com>
2025-08-25 20:48:04 +0530

    Leetcode: 0684-redundant-connection

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -2,33 +2,13 @@ class UnionFind():
 
     def __init__(self, n):
 
-        self.size = [1] * n
         self.parent = [x for x in range(n)]
-        self.no_of_sets = n
+        self.size = [1] * n
+        self.rem_sets = n
     
-    def union(self, p, q):
-
-        root1 = self.find(p)
-        root2 = self.find(q)
-
-        # if they are already in the same group then we don't have to do anything
-        if(root1 == root2):
-            return True
-        
-        if(self.size[root1] >= self.size[root2]):
-            self.size[root1] += self.size[root2]
-            self.parent[root2] = root1
-        else: 
-            self.size[root2] += self.size[root1]
-            self.parent[root1] = root2
-    
-        self.no_of_sets -= 1
-        return False
-        
     def find(self, u):
 
         root = u
-
         while(root != self.parent[root]):
             root = self.parent[root]
         
@@ -39,13 +19,33 @@ class UnionFind():
             curr_node = temp
         
         return root
+    
+    def union(self, u, v):
 
+        root1 = self.find(u)
+        root2 = self.find(v)
+
+        if(root1 == root2):
+            return True
+        
+        if(self.size[root1] > self.size[root2]):
+            self.size[root1] += self.size[root2]
+            self.parent[root2] = root1
+        else:
+            self.size[root2] += self.size[root1]
+            self.parent[root1] = root2
+
+        self.rem_sets -= 1
+        
 class Solution:
-    def findRedundantConnection(self, edges):
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
 
         obj = UnionFind(len(edges))
-
         for u,v in edges:
 
             if(obj.union(u-1,v-1)):
                 return [u,v]
+        
+
+            
+        
