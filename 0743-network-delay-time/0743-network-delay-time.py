29e52f46d59301bb690bdc1ee1ab83d1afb5de42
Vedhanth <gvedhanth123@outlook.com>
2025-09-29 15:01:53 +0530

    0743-network-delay-time

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,38 +1,45 @@
+from typing import List
+from collections import defaultdict
+import heapq
+
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         
 
-        # dijkstra is of 3 steps
+        distances = [float('inf')] * n
+        distances[k-1] = 0
 
-        # 1) initialize all the nodex = infintiy distance except the source node which is 0
-        # 2) Also take set for checked whether this node is visited or not
-        # 3) user priority queue to pick the minimum weight node
+        graph = defaultdict(list)
 
-        # 1) convert the graph to adjacency list 
-        g = defaultdict(list)
-        for u,v,w in times:
-            g[u].append((v,w))
+        for src, dest, weight in times:
+            graph[src-1].append((weight, dest-1))
         
-        distances = {x:float('inf') for x in range(1, n+1)}
+        priority_queue = [(0,k-1)]
 
-        distances[k] = 0
 
         visited = set()
 
-        queue = [(0,k)]
-        while(queue):
-            w,u = heapq.heappop(queue)
+        while(priority_queue):
 
-            if(u in visited):
+            weight, node = heapq.heappop(priority_queue)
+            curr_distance = distances[node]
+
+            if(node in visited):
                 continue
 
-            visited.add(u)
+            visited.add(node)
 
-            for v,w in g[u]:
-                distances[v] = min(distances[v], distances[u] + w)
-                heapq.heappush(queue,(distances[v],v))
+            for new_weight, neighbour in graph[node]:
+                # visited.add((curr_distance + weight, neighbour))
+                    distances[neighbour] = min(distances[neighbour],curr_distance + new_weight)
+                    heapq.heappush(priority_queue, (curr_distance + new_weight, neighbour))
 
-        result = max(list(distances.values()))
-        return result if result != float('inf') else -1
+        if(len(visited) != n):
+            return -1
+        else:
+            return max(distances)
 
+if __name__ == "__main__":
+    obj = Solution()
+    obj.networkDelayTime(times = [[1,2,1],[2,3,2],[1,3,2]], n = 3, k = 1)
 
