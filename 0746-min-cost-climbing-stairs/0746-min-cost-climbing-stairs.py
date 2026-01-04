43ecdf7a0d0e2e6149c5fb1997c38263105bf838
Vedhanth <gvedhanth123@outlook.com>
2026-01-04 15:16:06 +0530

    0746-min-cost-climbing-stairs 01-04-2026

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,22 +1,10 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         
-        memo = {}
-        def rec(n):
 
-            if(n in memo):
-                return memo[n]
+        dp = [cost[0], cost[1]] + [-1] * (len(cost) - 2)
 
-            if(n == len(cost)):
-                return 0
-            
-            # 1 step
-            oneStep = cost[n] + rec(n+1)
-            if(n < len(cost)-1):
-                twoStep = cost[n+1] + rec(n+2)
-            else:
-                twoStep = 0
-            memo[n] = min(oneStep, twoStep)
-            return memo[n]
+        for x in range(2, len(cost)):
+            dp[x] = cost[x] + min(dp[x-1], dp[x-2])
         
-        return rec(0)
+        return min(dp[-2:])
