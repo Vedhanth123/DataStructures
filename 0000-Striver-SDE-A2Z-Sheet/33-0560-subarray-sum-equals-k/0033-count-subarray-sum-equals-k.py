3aa1467373454ee44d119cd6df341aeea34e23f9
Vedhanth123 <gvedhanth123@outlook.com>
2026-02-11 10:44:27 +0000

    Find missing and repeating numbers

--- 0000-Striver-SDE-A2Z-Sheet/33-0560-subarray-sum-equals-k/0033-count-subarray-sum-equals-k.py
+++ 0000-Striver-SDE-A2Z-Sheet/33-0560-subarray-sum-equals-k/0033-count-subarray-sum-equals-k.py
@@ -1,5 +1,18 @@
-class Solution():
+from typing import List
 
-    def method(self, array: list[int]) -> int:
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        
+        sum_ = 0
+        count = 0   
+        hashMap = {0:1}
 
-        ...
+        for x in range(len(nums)):
+
+            sum_ += nums[x]
+            target = sum_ - x
+            if(target in hashMap):
+                count += hashMap[target]
+            hashMap[sum_] = hashMap.get(sum_, 1)
+        
+        return count
